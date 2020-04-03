c35d5bce45541d8c34b4cb3bbfa72ad6861b8417
daslef <daslef93@gmail.com>
2020-04-03 23:00:37 +0300

    basic realization with some enhancement. todo: fix 00 bug

--- 02_clock/src/index.js
+++ 02_clock/src/index.js
@@ -5,7 +5,7 @@ setInterval(() => {
     let hour = currentDate.getHours()
     let minute = currentDate.getMinutes()
     let second = currentDate.getSeconds()
-    hoursHand.style.transform = `translate(6.2rem, -15px) rotate(${hour * 30 + 90}deg)`
-    minutesHand.style.transform = `translateY(15px) rotate(${minute * 6 + 90}deg)`
-    secondsHand.style.transform = `translateY(15px) rotate(${second * 6 + 90}deg)`
+    hoursHand.style.transform = `translateX(6.2rem) rotate(${hour * 30 + 90}deg)`
+    minutesHand.style.transform = `rotate(${minute * 6 + 90}deg)`
+    secondsHand.style.transform = `rotate(${second * 6 + 90}deg)`
 }, 1000)
