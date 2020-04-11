d4ce7eba449561ecc497de898d3ca7c022d00b15
daslef <daslef93@gmail.com>
2020-04-11 18:56:36 +0300

    a lot of improvements

--- 11_custom_video_player/index.js
+++ 11_custom_video_player/index.js
@@ -1,20 +1,23 @@
 const videoSrc = ['assets/foggy_x264.mp4', 'assets/ocean_x264.mp4', 'assets/ophtal_x264.mp4']
 let currentVideoIx = 1
+let expanded = false
 
 const canvas = document.querySelector('canvas');
 const wrapper = document.querySelector('.video-player');
 const video = document.querySelector('.video-player__video');
-const ui = document.querySelector('.video-player__ui');
-const playButton = document.querySelector('.video-player__play-pause')
+
+let WIDTH, HEIGHT
+
 const progressWrapper = document.querySelector('.video-player__progress-wrapper')
 const progressBar = document.querySelector('.video-player__progress-bar')
 const menu = document.querySelector('.video-player__menu')
 
 const timing = document.querySelector('.remote__time')
-const remoteForward = document.querySelector('.remote__forward')
-const remoteBackward = document.querySelector('.remote__backward')
+const remoteForward = document.querySelector('g#Forward')
+const remoteBackward = document.querySelector('g#Backward')
 const remotePlay = document.querySelector('g#Play')
 const remoteMenu = document.querySelector('g#Menu')
+const remoteExpand = document.querySelector('g#Expand')
 
 let menuMode = false
 
@@ -33,14 +36,14 @@ function calculateMean(data) {
 }
 
 function processImage(data) {
-    let top = data.slice(0, video.videoWidth * 4 * 80).filter((value, index) => (index+1) % 4 !== 0)
-    let bottom = data.slice(data.length - 80 * 4 * video.videoWidth).filter((value, index) => (index+1) % 4 !== 0)
+    let top = data.slice(0, WIDTH * 4 * 20).filter((value, index) => (index+1) % 4 !== 0)
+    let bottom = data.slice(data.length - 20 * 4 * WIDTH).filter((value, index) => (index+1) % 4 !== 0)
 
     let left = [], right = []
-    for (let i=0, j=data.length; i<j; i+=video.videoWidth*4) {
-        let temporary = data.slice(i, i + video.videoWidth*4).filter((value, index) => (index+1) % 4 !== 0),
-            temporary_left = temporary.filter((value, index) => (index < 40)),
-            temporary_right = temporary.slice().reverse().filter((value, index) => (index < 40))
+    for (let i=0, j=data.length; i<j; i+=WIDTH*4) {
+        let temporary = data.slice(i, i + WIDTH*4).filter((value, index) => (index+1) % 4 !== 0),
+            temporary_left = temporary.filter((value, index) => (index < 15)),
+            temporary_right = temporary.slice().reverse().filter((value, index) => (index < 15))
         left.push(...temporary_left)
         right.push(...temporary_right)
     }
@@ -48,25 +51,36 @@ function processImage(data) {
 }
 
 function capture() {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-    let frame = ctx.getImageData(0, 0, video.videoWidth, video.videoHeight);
+    canvas.width = WIDTH
+    canvas.height = HEIGHT
+    ctx.drawImage(video, 0, 0, WIDTH, HEIGHT);
+    let frame = ctx.getImageData(0, 0, WIDTH, HEIGHT);
     let [top_mean, bottom_mean, left_mean, right_mean] = processImage(frame.data)
-    let [top_mean_r, top_mean_g, top_mean_b] = top_mean,
-        [bottom_mean_r, bottom_mean_g, bottom_mean_b] = bottom_mean,
-        [left_mean_r, left_mean_g, left_mean_b] = left_mean,
-        [right_mean_r, right_mean_g, right_mean_b] = right_mean
+
+    for (let x of [top_mean, bottom_mean]) {
+        console.log(`%c ${'❚'.repeat(30)};`, `color: rgb(${x[0]}, ${x[1]}, ${x[2]})`)
+    }
+
+    canvas.style.border = "4px solid"
+    canvas.style.borderTopColor = `rgba(${top_mean.join(',')})`
+    canvas.style.borderBottomColor = `rgba(${bottom_mean.join(',')})`
+    canvas.style.borderLeftColor = `rgba(${left_mean.join(',')})`
+    canvas.style.borderRightColor = `rgba(${right_mean.join(',')})`
+
+    let topShadowLong = `0px -15px 40px 2px rgba(${top_mean.join(',')}, 0.95)`
+    let topShadowShort = `0px -5px 5px -4px rgba(${top_mean.join(',')}, 0.95)`
+
+    let bottomShadowLong = `0px 15px 40px 2px rgba(${bottom_mean.join(',')}, 0.95)`
+    let bottomShadowShort = `0px 5px 5px -4px rgba(${bottom_mean.join(',')}, 0.95)`
+
+    let leftShadowShort = `-2px 0px 10px -4px rgba(${left_mean.join(',')}, 0.85)`
+    let rightShadowShort = `2px 0px 10px -4px rgba(${right_mean.join(',')}, 0.85)`
+
+    let allShadowLong = `${topShadowLong}, ${bottomShadowLong}`
+    let allShadowShort = `${topShadowShort}, ${bottomShadowShort}, ${leftShadowShort}, ${rightShadowShort}`
+
     for (let prop of ["-webkit-box-shadow", "-moz-box-shadow", "box-shadow"]) {
-        wrapper.style.setProperty(prop, `6px -4px 4px 0 black,
-                                                0px -20px 35px 5px rgba(${top_mean_r}, ${top_mean_g}, ${top_mean_b}, 0.75),
-                                               0px -10px 15px -4px rgba(${top_mean_r}, ${top_mean_g}, ${top_mean_b}, 0.85),
-                                               0px 20px 35px 5px rgba(${bottom_mean_r}, ${bottom_mean_g}, ${bottom_mean_b}, 0.75),
-                                               0px 10px 15px -4px rgba(${bottom_mean_r}, ${bottom_mean_g}, ${bottom_mean_b}, 0.85),
-                                               -15px 0px 20px 5px rgba(${left_mean_r}, ${left_mean_g}, ${left_mean_b}, 0.75),
-                                               -5px 5px 10px -4px rgba(${left_mean_r}, ${left_mean_g}, ${left_mean_b}, 0.85),
-                                               15px 0px 20px 5px rgba(${right_mean_r}, ${right_mean_g}, ${right_mean_b}, 0.75),
-                                               5px 5px 10px -4px rgba(${right_mean_r}, ${right_mean_g}, ${right_mean_b}, 0.85)`)
+        wrapper.style.setProperty(prop, `5px -3px 3px 0 black, ${allShadowLong}, ${allShadowShort}`)
     }
 }
 
@@ -76,17 +90,6 @@ function changeTimingHandler(e) {
     video.currentTime = Math.ceil(percent * video.duration)
 }
 
-function changeSpeedHandler() {
-    video.playbackRate = parseFloat(this.value)
-}
-
-function pauseHandler() {
-    playButton.innerHTML = '<i class="fa fa-play"></i>'
-}
-
-function playHandler() {
-    playButton.innerHTML = '<i class="fa fa-pause"></i>'
-}
 
 function updateTime() {
     let date = new Date('1993-02-22T00:00:00');
@@ -159,20 +162,41 @@ function menuButtonHandler() {
     menu.classList.toggle('video-player__menu_active')
 }
 
+function expandButtonHandler() {
+    if (!expanded) {
+        video.style.width = "1280px";
+        wrapper.style.left = `${(window.innerWidth - 1280) / 2 - 100}px`
+        wrapper.style.top = `${(window.innerHeight - 720) / 2}px`
+        document.body.style.filter = "opacity: 0.8"
+        document.body.style.backgroundSize = "210%"
+        document.body.style.backgroundPositionY = "41%"
+        document.querySelector('svg').style.right = '2vw'
+    } else {
+        video.style.width = "380px"
+        wrapper.style.left = "40vw"
+        wrapper.style.top = "26vh"
+        document.body.style.filter = "opacity: 1"
+        document.body.style.backgroundSize = "cover"
+        document.body.style.backgroundPositionY = "center"
+        document.querySelector('svg').style.right = '5vw'
+    }
+    expanded = !expanded
+}
+
 video.addEventListener('loadedmetadata', () => {
     durationMinutes = Math.floor(video.duration / 60)
     durationSeconds = Math.floor(video.duration % 60)
+    WIDTH = 180
+    HEIGHT = 180 * video.videoHeight / video.videoWidth
 })
 
 video.addEventListener('timeupdate', updateHandler)
 video.addEventListener('click', playButtonHandler)
-video.addEventListener('play', playHandler)
-video.addEventListener('pause', pauseHandler)
 
-playButton.addEventListener('click', playButtonHandler)
 progressWrapper.addEventListener('click', changeTimingHandler)
 
 remoteForward.addEventListener('click', forwardButtonHandler)
 remoteBackward.addEventListener('click', backwardButtonHandler)
 remotePlay.addEventListener('click', playButtonHandler)
 remoteMenu.addEventListener('click', menuButtonHandler)
+remoteExpand.addEventListener('click', expandButtonHandler)
