6d791e468672514efe6324a2445badf9dacb20bd
daslef <daslef93@gmail.com>
2020-04-23 18:47:01 +0300

    sticky nav: work with infinite scrolling continued

--- 24_sticky_nav/index.js
+++ 24_sticky_nav/index.js
@@ -1,28 +1,18 @@
+const url = 'https://raw.githubusercontent.com/daslef/js-30/master/24_sticky_nav/data.json'
+
 const nav = document.querySelector('nav')
+const main = document.querySelector('main')
+const footer = document.querySelector('footer')
 let topOfNav = nav.offsetTop;
 
-const recipeIntroduction = document.querySelector('.recipe__introduction')
-const recipeNutrients = document.querySelector('.recipe__nutrients')
-const recipeIngredients = document.querySelector('.recipe__ingredients')
-const recipeInstructions = document.querySelector('.recipe__instructions')
-const recipeSections = [recipeIntroduction, recipeNutrients, recipeIngredients, recipeInstructions]
-
 const recipeMenuIntroduction = document.querySelector('.recipe__introduction__menu')
-const recipeMenuNutrients = document.querySelector('.recipe__nutrients__menu')
 const recipeMenuIngredients = document.querySelector('.recipe__ingredients__menu')
 const recipeMenuInstructions = document.querySelector('.recipe__instructions__menu')
 const recipeMenuOrder = document.querySelector('.recipe__order__menu')
-const recipeMenuItems = [recipeMenuIntroduction, recipeMenuNutrients, recipeMenuIngredients, recipeMenuInstructions, recipeMenuOrder]
+const recipeMenuItems = [recipeMenuIntroduction, recipeMenuIngredients, recipeMenuInstructions, recipeMenuOrder]
 
-window.addEventListener('scroll', () => {
 
-    if (this.scrollY >= topOfNav) {
-        document.body.style.paddingTop = nav.offsetHeight + 'px'
-        document.body.classList.add('fixed-nav')
-    } else {
-        document.body.classList.remove('fixed-nav')
-        document.body.style.paddingTop = 0
-    }
+function setActiveMenuItem() {
 
     let x = this.innerWidth / 2
     let y = this.innerHeight / 3
@@ -40,13 +30,6 @@ window.addEventListener('scroll', () => {
             recipeMenuIntroduction.classList.add('highlighted')
             break;
 
-        case 'recipe__nutrients':
-            recipeMenuItems.filter(x => x !== recipeMenuNutrients).map(x => {
-                x.classList.remove('highlighted')
-            })
-            recipeMenuNutrients.classList.add('highlighted')
-            break;
-
         case 'recipe__ingredients':
             recipeMenuItems.filter(x => x !== recipeMenuIngredients).map(x => {
                 x.classList.remove('highlighted')
@@ -61,40 +44,67 @@ window.addEventListener('scroll', () => {
             recipeMenuInstructions.classList.add('highlighted')
             break;
     }
+}
 
-})
 
-function newRecipe() {
-
+function newRecipeElement(recipe) {
     let newRecipe = document.createElement('div')
     newRecipe.classList.add('recipe')
 
-    const url = ''
+    let ingredients = recipe.ingredients
+        .map(el => `<li>${el}</li>`)
+        .join('')
+
+    let instruction = recipe.instruction
+        .map(el => `<li>${el}</li>`)
+        .join('')
+
+    newRecipe.innerHTML = `
+                <div class="recipe__introduction">
+                    <div class="recipe__title">${recipe.title}</div>
+                    <p>ВРЕМЯ ПРИГОТОВЛЕНИЯ - ${recipe.duration} ЧАС</p>
+                    <img src="${recipe.imgSrc}">
+                    <div class="recipe__nutrients">
+                        <ul>ЭНЕРГЕТИЧЕСКАЯ ЦЕННОСТЬ НА ПОРЦИЮ</ul>
+                        <li>КАЛОРИЙНОСТЬ - ${recipe.nutrients['kkal']} ККАЛ</li>
+                        <li>БЕЛКИ - ${recipe.nutrients['protein']} ГРАММ</li>
+                        <li>ЖИРЫ ${recipe.nutrients['fat']} ГРАММ</li>
+                        <li>УГЛЕВОДЫ ${recipe.nutrients['carbo']} ГРАММ</li>
+                    </div>
+                </div>
+                <div class="recipe__ingredients">
+                    <ul>ИНГРЕДИЕНТЫ</ul>
+                    ${ingredients}
+                </div>
+                <div class="recipe__instructions">
+                    <ul>ИНСТРУКЦИЯ</ul>
+                    ${instruction}
+                </div>`
+    return newRecipe
+}
+
+
+function updateRecipes() {
     fetch(url)
         .then(response => response.json())
         .then(data => data[0])
-        .then(recipe => {
-            newRecipe.innerHTML = `
-            <div class="recipe__title">${recipe.title}</div>
-            <div class="recipe__introduction">
-                <p>ВРЕМЯ ПРИГОТОВЛЕНИЯ - ${recipe.duration} ЧАС</p>
-                <img src="${recipe.imgSrc}">
-            </div>
-            <div class="recipe__nutrients">
-                <ul>ЭНЕРГЕТИЧЕСКАЯ ЦЕННОСТЬ НА ПОРЦИЮ</ul>
-                <li>КАЛОРИЙНОСТЬ - ${recipe.nutrients['kkal']} ККАЛ</li>
-                <li>БЕЛКИ - ${recipe.nutrients['protein']} ГРАММ</li>
-                <li>ЖИРЫ ${recipe.nutrients['fat']} ГРАММ</li>
-                <li>УГЛЕВОДЫ ${recipe.nutrients['carbo']} ГРАММ</li>
-            </div>
-            <div class="recipe__ingredients">
-                <ul>ИНГРЕДИЕНТЫ</ul>
-            </div>
-            <div class="recipe__instructions">
-                <ul>ИНСТРУКЦИЯ</ul>
-            </div>`
-            }
-        )
-    console.log(newRecipe)
-
+        .then(recipe => main.appendChild(newRecipeElement(recipe)))
+        .catch(e => console.log(e))
 }
+
+
+const options = {root: null, rootMargins: "0px", threshold: 0.5}
+const observer = new IntersectionObserver(updateRecipes, options)
+observer.observe(footer)
+
+
+window.addEventListener('scroll', () => {
+    if (this.scrollY >= topOfNav) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px'
+        document.body.classList.add('fixed-nav')
+    } else {
+        document.body.classList.remove('fixed-nav')
+        document.body.style.paddingTop = 0
+    }
+    setActiveMenuItem()
+})
